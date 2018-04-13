9eab8be722c0af76b088a9f484e26619a017a84c
Matt McKenna <matt.c.mckenna@gmail.com>
2018-04-13 02:23:49 -0400

    Added MS AppCenter

--- app/src/main/java/io/github/matthewcmckenna/owl/nano/android/NanoApplication.kt
+++ app/src/main/java/io/github/matthewcmckenna/owl/nano/android/NanoApplication.kt
@@ -1,6 +1,9 @@
 package io.github.matthewcmckenna.owl.nano.android
 
 import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import io.github.matthewcmckenna.owl.nano.BuildConfig
 import io.github.matthewcmckenna.owl.nano.logging.timber.NanoDebugTree
 import io.github.matthewcmckenna.owl.nano.logging.timber.NoLogTree
@@ -10,6 +13,12 @@ class NanoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        AppCenter.start(
+                this,
+                "",
+                Analytics::class.java,
+                Crashes::class.java)
 
         if (BuildConfig.DEBUG) {
             Timber.plant(NanoDebugTree())
--- app/src/main/java/io/github/matthewcmckenna/owl/nano/logging/timber/CrashReportingTree.kt
+++ app/src/main/java/io/github/matthewcmckenna/owl/nano/logging/timber/CrashReportingTree.kt
@@ -1,20 +1,42 @@
 package io.github.matthewcmckenna.owl.nano.logging.timber
 
 import android.util.Log
+import com.microsoft.appcenter.analytics.Analytics
 import timber.log.Timber
 
 class CrashReportingTree : Timber.Tree() {
+    init {
+        Analytics.setEnabled(true)
+    }
+
     override fun log(priority: Int, tag: String?, message: String?, t: Throwable?) {
         if (priority == Log.VERBOSE || priority == Log.DEBUG) {
             return
         }
-//        TODO("Crash Library").log(priority, tag, message);
-//        if (t != null) {
-//            if (priority == Log.ERROR) {
-//                TODO("Crash Library")Library.logError(t)
-//            } else if (priority == Log.WARN) {
-//                TODO("Crash Library").logErrorWarning(t)
-//            }
-//        }
+
+        if (t != null) {
+            val properties = mutableMapOf<String, String>()
+            properties["priority"] = priority.toString()
+            properties["priority_text"] = priorityToDescriptor(priority)
+            tag?.let { properties.put("tag", it) }
+            message?.let { properties.put("tag", it) }
+
+            if (priority == Log.ERROR) {
+//                Crashes.trackException(t, properties)
+            } else if (priority == Log.WARN) {
+//                Crashes.trackException(t, properties)
+            }
+        }
     }
+
+    private fun priorityToDescriptor(priority: Int) =
+            when (priority) {
+                Log.VERBOSE -> "Verbose"
+                Log.DEBUG -> "Debug"
+                Log.INFO -> "Info"
+                Log.WARN -> "Warn"
+                Log.ERROR -> "Error"
+                Log.ASSERT -> "Assert"
+                else -> "Unknown Priority: $priority"
+            }
 }
