4fdb831f3e2495cb37d24262ae489eaba9f03d8b
Matt McKenna <matt.c.mckenna@gmail.com>
2018-04-25 21:42:28 -0400

    add links in debug logs
    Can now click reference to jump to where the log is

--- app/src/main/java/io/github/matthewcmckenna/owl/nano/logging/timber/NanoDebugTree.kt
+++ app/src/main/java/io/github/matthewcmckenna/owl/nano/logging/timber/NanoDebugTree.kt
@@ -1,12 +1,11 @@
 package io.github.matthewcmckenna.owl.nano.logging.timber
 
 import timber.log.Timber
+import java.util.*
+import java.util.regex.Pattern
 
 class NanoDebugTree : Timber.DebugTree() {
-
-    override fun createStackElementTag(element: StackTraceElement?): String {
-        return String.format("C:%s:%s",
-                super.createStackElementTag(element),
-                element?.lineNumber)
+    override fun createStackElementTag(element: StackTraceElement): String {
+        return String.format(Locale.getDefault(), "(%s:%d)", element.fileName, +element.lineNumber)
     }
 }
