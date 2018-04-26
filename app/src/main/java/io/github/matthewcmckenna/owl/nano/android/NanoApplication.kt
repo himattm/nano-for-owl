d1f57b4e810e821ea03257022310a5ba8fc52713
Matt McKenna <matt.c.mckenna@gmail.com>
2018-04-25 21:57:03 -0400

    update secret push to fix build

--- app/src/main/java/io/github/matthewcmckenna/owl/nano/android/NanoApplication.kt
+++ app/src/main/java/io/github/matthewcmckenna/owl/nano/android/NanoApplication.kt
@@ -5,7 +5,7 @@ import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 import io.github.matthewcmckenna.owl.nano.BuildConfig
-import io.github.matthewcmckenna.owl.nano.core.secrets.APP_CENTER_SECRET
+import io.github.matthewcmckenna.owl.nano.R
 import io.github.matthewcmckenna.owl.nano.logging.timber.NanoDebugTree
 import io.github.matthewcmckenna.owl.nano.logging.timber.NoLogTree
 import timber.log.Timber
@@ -17,7 +17,7 @@ class NanoApplication : Application() {
 
         AppCenter.start(
                 this,
-                APP_CENTER_SECRET,
+                getString(R.string.appcenter_client_secret),
                 Analytics::class.java,
                 Crashes::class.java)
 
